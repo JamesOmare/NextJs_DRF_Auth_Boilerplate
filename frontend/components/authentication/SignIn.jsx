89442b9dd484f26459da63a4c32c3c74afb7c35d
james <jamesomare922@gmail.com>
2024-08-07 23:55:05 +0300

    added login page

---
+++ frontend/components/authentication/SignIn.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import { useAuth } from "../hooks/AuthProvider";
+
+const Login = () => {
+  const [input, setInput] = useState({
+    username: "",
+    password: "",
+  });
+
+  const auth = useAuth();
+  const handleSubmitEvent = (e) => {
+    e.preventDefault();
+    if (input.username !== "" && input.password !== "") {
+      auth.loginAction(input);
+      return;
+    }
+    alert("pleae provide a valid input");
+  };
+
+  return (
+    <form onSubmit={handleSubmitEvent}>
+      {/* Form inputs are provided in the above examples */}
+    </form>
+  );
+};
+
+export default Login;
